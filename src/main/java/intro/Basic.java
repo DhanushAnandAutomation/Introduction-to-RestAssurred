f92f849146473a32b478a399543b4f88e4600da9
DhanushAnandAutomation <dhanushanand@outlook.com>
2025-05-18 19:45:39 +0530

    Basic API testing ADD

--- src/main/java/intro/Basic.java
+++ src/main/java/intro/Basic.java
@@ -1,35 +1,44 @@
 package intro;
 
 import io.restassured.RestAssured;
+import static org.hamcrest.Matchers.*;
+
+import files.Payload;
+
 import static io.restassured.RestAssured.*;
 
 public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//to validate ADD place API is working as expected
 		
-		
+		//setting base URL
 		RestAssured.baseURI="https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Dhanush house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}\r\n"
-				+ "")
+		
+		//given is used to start the "Request". 
+		//giving Query parameter in Endpoint Request
+		given().log().all().queryParam("key", "qaclick123")
+		
+		//giving header in Endpoint Request
+		.header("Content-Type", "application/json")
+		
+		//giving body in header Request
+		.body(Payload.addPlace())
+		
+		//giving resources in Header Request
 		.when().post("maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200);
+		
+		//"then" is used to read the response
+		//assertion is done to check status code in response is equal to 200
+		.then().log().all().assertThat().statusCode(200)
+		
+		//equalTo function is used to assert scope of response is APP
+		.body("scope",  equalTo("APP"))
+		
+		//header is again used when it is for a response
+		//is used to assert server which the response is coming to is "Apache/2.4.52 (Ubuntu)"
+		.header("server", "Apache/2.4.52 (Ubuntu)");
 
 	}
 
